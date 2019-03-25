a6e3f2ad1ce9f7e5a9cd23de46f2a51fbc591048
Daniele Zanaga <daniele.zanaga@vito.be>
2019-03-25 15:52:29 +0100

    Update

--- start-eojup.sh
+++ start-eojup.sh
@@ -3,24 +3,22 @@
 # Script reference:
 # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html
 
-IMAGE="eojup"
-# 1145fb1198b2 
-# CONTAINER_NAME="eojupy"
-CONTAINER_NAME="jupyter/scipy-notebook:1145fb1198b2"
+IMAGE="redblanket/eojup:latest"
+
+CONTAINER_NAME="eojupy"
 
 NB_USERNAME="jovyan" # Changing this leads to errors... to be tested
 
-# HOST_WORK="/home/`id -u -n`/repos/"
-# CONTAINER_WORK="/home/$NB_USERNAME/repos"
+HOST_WORK="/home/`id -u -n`/repos/"
+CONTAINER_WORK="/home/$NB_USERNAME/repos"
 
-# DATA="/data/"
-# DATA_MOUNT="/data/"
+DATA="/data/"
+DATA_MOUNT="/data/"
 
-DATA="/c/Users/ZANAGAD/Downloads"
-DATA_MOUNT="/home/$NB_USERNAME/Downloads"
+## Local
+# DATA="/c/Users/ZANAGAD/Downloads"
+# DATA_MOUNT="/home/$NB_USERNAME/Downloads"
 
-# -v $HOST_WORK:$CONTAINER_WORK \
-# -v $MEP_DATA:$MEP_DATA_MOUNT \
 
 JUPYTER_TOKEN="d920926ebf16df183c9cbbddbe15966d5798902567a75ab3"
 # "c8de56fa4deed24899803e93c227592aef6538f93025fe01"
@@ -29,10 +27,12 @@ JUPYTER_TOKEN="d920926ebf16df183c9cbbddbe15966d5798902567a75ab3"
 # >>> IPython.lib.passwd()
 # HASHED_PASSWD="'sha1:67a75ab3faee:d920926ebf16df183c9cbbddbe15966d57989025'"  # vito
 
-docker run --rm \
+docker run -d \
+  --restart always \
   --name $CONTAINER_NAME \
-  -p 10000:8888 \
+  -p 9999:8888 \
   -v ${DATA}:${DATA_MOUNT} \
+  -v ${HOST_WORK}:${CONTAINER_WORK} \
   -e NB_UID=`id -u` \
   -e NB_GID=`id -g` \
   -e GRANT_SUDO=yes \
